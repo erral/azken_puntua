a8bb6c1b3f2e043866a37b79dc0a2dd205f8a9f5
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-08-26 21:45:11 +0200

    code

---
+++ toot_item.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+This script is inspired by https://github.com/ZiTAL/bermiotarra/tree/master/bot by @ZiTAL
+"""
+import json
+import datetime
+import random
+from mastodon import Mastodon
+
+
+def toot_item_from_list():
+    """ main function to toot items """
+    item = get_random_item_to_toot()
+    if item:
+        toot_item(item)
+
+
+def get_random_item_to_toot():
+    """ select an untooted item """
+    with open("azken_puntuak.json", "r") as fp:
+        dictionary = json.load(fp)
+        word = random.choice(dictionary)
+        while word and word.get("tweeted"):
+            word = random.choice(dictionary)
+
+    word["tweeted"] = True
+    word["tweeted_at"] = datetime.datetime.utcnow().isoformat()
+    dictionary.remove(word)
+    dictionary.append(word)
+
+    with open("azken_puntuak.json", "w") as fp:
+        json.dump(dictionary, fp)
+
+    return word
+
+
+def toot_item(item):
+    """ effectively toot the text """
+    text = "{text} {singer} {url}"
+    text = text.format(
+        text=item['text'],
+        singer=item['singer'],
+        url=item['url']
+    )
+
+    text = text.strip()
+
+    if text:
+
+        with open("credentials.mastodon.json") as fp:
+            credentials = json.load(fp)
+
+            api = Mastodon(
+                access_token=credentials["ACCESS_TOKEN"],
+                api_base_url="https://mastodon.eus"
+            )
+            res = api.toot(text)
+            if res.get('id'):
+                print('Tooted: "{}"'.format(text))
+
+
+if __name__ == "__main__":
+    toot_item_from_list()
