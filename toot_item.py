cf1e4e643af36d04aab38d1113d05d91d8caf701
Mikel Larreategi <mlarreategi@codesyntax.com>
2024-08-27 16:45:34 +0200

    format

--- toot_item.py
+++ toot_item.py
@@ -9,14 +9,14 @@ from mastodon import Mastodon
 
 
 def toot_item_from_list():
-    """ main function to toot items """
+    """main function to toot items"""
     item = get_random_item_to_toot()
     if item:
         toot_item(item)
 
 
 def get_random_item_to_toot():
-    """ select an untooted item """
+    """select an untooted item"""
     with open("azken_puntuak.json", "r") as fp:
         dictionary = json.load(fp)
         word = random.choice(dictionary)
@@ -35,13 +35,9 @@ def get_random_item_to_toot():
 
 
 def toot_item(item):
-    """ effectively toot the text """
-    text = "{text} {singer} {url}"
-    text = text.format(
-        text=item['text'],
-        singer=item['singer'],
-        url=item['url']
-    )
+    """effectively toot the text"""
+    text = "{text} \n\n{singer}\n{url}"
+    text = text.format(text=item["text"], singer=item["singer"], url=item["url"])
 
     text = text.strip()
 
@@ -52,10 +48,10 @@ def toot_item(item):
 
             api = Mastodon(
                 access_token=credentials["ACCESS_TOKEN"],
-                api_base_url="https://mastodon.eus"
+                api_base_url="https://mastodon.eus",
             )
             res = api.toot(text)
-            if res.get('id'):
+            if res.get("id"):
                 print('Tooted: "{}"'.format(text))
 
 
